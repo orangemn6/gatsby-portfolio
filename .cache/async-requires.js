57e2baa43770dac43ac6fd25dffc9dc20588acb7
orangemn6 <jbensa081@gmail.com>
2021-02-13 16:39:36 -0500

    first

---
+++ .cache/async-requires.js
@@ -0,0 +1,15 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-blog-js": () => import("./../../../src/pages/blog.js" /* webpackChunkName: "component---src-pages-blog-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-repositories-js": () => import("./../../../src/pages/repositories.js" /* webpackChunkName: "component---src-pages-repositories-js" */),
+  "component---src-pages-works-js": () => import("./../../../src/pages/works.js" /* webpackChunkName: "component---src-pages-works-js" */),
+  "component---src-templates-blogs-categories-js": () => import("./../../../src/templates/blogs/categories.js" /* webpackChunkName: "component---src-templates-blogs-categories-js" */),
+  "component---src-templates-blogs-post-js": () => import("./../../../src/templates/blogs/post.js" /* webpackChunkName: "component---src-templates-blogs-post-js" */),
+  "component---src-templates-works-work-js": () => import("./../../../src/templates/works/work.js" /* webpackChunkName: "component---src-templates-works-work-js" */)
+}
+
---
+++ src/components/about/index.jsx
@@ -0,0 +1,41 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import {AboutSection, Avatar, Title, Text, SubTitle} from './style';
+import {SectionIntro, ContainerLayout, ResumeButton} from "../common";
+
+const About = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "profile.webp" }) {
+        childImageSharp {
+          fluid(maxWidth: 550) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <>
+      <SectionIntro>
+        <ContainerLayout>
+          <AboutSection>
+            <div>
+              <Avatar fluid={data.placeholderImage.childImageSharp.fluid} alt="user photo" />
+              <SubTitle> Web Developer</SubTitle>
+            </div> 
+            <div>
+              <Title> Hello, I’m Orangemn6 </Title>
+              <Text> I'm a digital Front End Developer hailing from <b className="text-primary lined-link">America</b></Text>
+              <Text> I love working with modern technologies, building and designing awesome projects. I prefer minimalistic & clean designs with strong user experience.</Text>
+              <ResumeButton href="resume.pdf" target="_blank"> Download resume </ResumeButton>
+            </div>
+          </AboutSection>
+        </ContainerLayout>
+      </SectionIntro>
+    </>
+  )
+}
+
+
+export default About
