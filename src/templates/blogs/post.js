19ed6cc5c96c46e422ec31791c7ed2022ca6f814
orangemn6 <jbensa081@gmail.com>
2021-02-14 23:07:38 -0500

    first

--- src/templates/blogs/post.js
+++ src/templates/blogs/post.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import Comments from "../../components/Comments"
 import { Calendar, Clock } from 'react-feather'
 
 import {Intro, Title, ArticlePost, SmallText, ArticleBody, NaviagtionList, NaviagtionLi } from '../../components/styled/posts'
