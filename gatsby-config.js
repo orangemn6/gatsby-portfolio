a8884fc44dd57880762ff79312b654821739c568
orangemn6 <jbensa081@gmail.com>
2021-02-15 23:19:59 -0500

    added post script

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
